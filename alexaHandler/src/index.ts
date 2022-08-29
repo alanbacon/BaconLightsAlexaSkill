bc5a8f883c7db496fe9fdd175ad72840d9f8aecd
Alan Bacon <alan@privateworkemail.com>
2022-08-30 00:17:39 +0100

    minor refactor and extra logging

--- alexaHandler/src/index.ts
+++ alexaHandler/src/index.ts
@@ -53,14 +53,89 @@ function getBearerTokenFromRequest(request: Alexa.API.Request): string {
   return bearerToken;
 }
 
+async function handleAuthorization(
+  request: Alexa.API.Request,
+  context: Alexa.API.RequestContext,
+): Promise<void> {
+  // Send the AcceptGrant response
+  const bearerToken = getBearerTokenFromRequest(request);
+  const requestCode = request.directive.payload.grant?.code || '';
+  await generateReturnChannelAccessToken(bearerToken, requestCode);
+  context.succeed(generateAcceptGrantResponse());
+}
+
+function handleDiscovery(
+  _request: Alexa.API.Request,
+  context: Alexa.API.RequestContext,
+): void {
+  // Send the discovery response
+  const resp = generateDiscoveryResponse();
+  log('DEBUG', 'Discovery Response: ', JSON.stringify(resp));
+  context.succeed(resp);
+}
+
+async function handlePowerControl(
+  request: Alexa.API.Request,
+  context: Alexa.API.RequestContext,
+): Promise<void> {
+  // get device ID passed in during discovery
+  const requestMethod = request.directive.header.name;
+  // get user token pass in request
+  const requestToken = getBearerTokenFromRequest(request);
+  const endpointId = request.directive.endpoint?.endpointId || '';
+  const roomName = getRoomNameFromEndpointId(endpointId);
+  if (!roomName) {
+    throw new Error('unable to map endpointId to room name');
+  }
+  let powerResult: 'ON' | 'OFF';
+
+  if (requestMethod === 'TurnOn') {
+    // Make the call to your device cloud for control
+    // powerResult = stubControlFunctionToYourCloud(endpointId, token, request);
+    await switchPowerOn(roomName);
+    powerResult = 'ON';
+  } else {
+    // Make the call to your device cloud for control and check for success
+    // powerResult = stubControlFunctionToYourCloud(endpointId, token, request);
+    await switchPowerOff(roomName);
+    powerResult = 'OFF';
+  }
+  const response = generatePowerUpdateResp(
+    powerResult,
+    endpointId,
+    requestToken,
+  );
+  log('DEBUG', 'Alexa.PowerController ', JSON.stringify(response));
+  context.succeed(response);
+}
+
+async function handleStateReport(
+  request: Alexa.API.Request,
+  context: Alexa.API.RequestContext,
+): Promise<void> {
+  const correlationToken = request.directive.header.messageId;
+  const endpointId = request.directive.endpoint?.endpointId || '';
+  const roomName = getRoomNameFromEndpointId(endpointId);
+  if (!roomName) {
+    throw new Error('unable to map endpointId to room name');
+  }
+  const roomState = await getRoomState(roomName);
+
+  const response = generateStateReportResponse(
+    roomState,
+    endpointId,
+    correlationToken,
+  );
+  context.succeed(response);
+}
+
 export async function handler(
   request: Alexa.API.Request,
   context: Alexa.API.RequestContext,
 ): Promise<void> {
-  console.log(JSON.stringify({ request }, null, 2));
-
   if (!(await isVerifiedUser(getBearerTokenFromRequest(request)))) {
     const unauthResp = generateInvalidAuthCredResponse();
+    log('DEBUG:', 'unauthorised response:', JSON.stringify(unauthResp));
     context.succeed(unauthResp);
   } else if (
     request.directive.header.namespace === 'Alexa.Discovery' &&
@@ -91,79 +166,6 @@ export async function handler(
     log('DEBUG:', 'unhandled request ', JSON.stringify(request));
   }
 
-  async function handleAuthorization(
-    request: Alexa.API.Request,
-    context: Alexa.API.RequestContext,
-  ): Promise<void> {
-    // Send the AcceptGrant response
-    const bearerToken = getBearerTokenFromRequest(request);
-    const requestCode = request.directive.payload.grant?.code || '';
-    await generateReturnChannelAccessToken(bearerToken, requestCode);
-    context.succeed(generateAcceptGrantResponse());
-  }
-
-  function handleDiscovery(
-    _request: Alexa.API.Request,
-    context: Alexa.API.RequestContext,
-  ): void {
-    // Send the discovery response
-    const resp = generateDiscoveryResponse();
-    log('DEBUG', 'Discovery Response: ', JSON.stringify(resp));
-    context.succeed(resp);
-  }
-
-  async function handlePowerControl(
-    request: Alexa.API.Request,
-    context: Alexa.API.RequestContext,
-  ): Promise<void> {
-    // get device ID passed in during discovery
-    const requestMethod = request.directive.header.name;
-    // get user token pass in request
-    const requestToken = getBearerTokenFromRequest(request);
-    const endpointId = request.directive.endpoint?.endpointId || '';
-    const roomName = getRoomNameFromEndpointId(endpointId);
-    if (!roomName) {
-      throw new Error('unable to map endpointId to room name');
-    }
-    let powerResult: 'ON' | 'OFF';
-
-    if (requestMethod === 'TurnOn') {
-      // Make the call to your device cloud for control
-      // powerResult = stubControlFunctionToYourCloud(endpointId, token, request);
-      await switchPowerOn(roomName);
-      powerResult = 'ON';
-    } else {
-      // Make the call to your device cloud for control and check for success
-      // powerResult = stubControlFunctionToYourCloud(endpointId, token, request);
-      await switchPowerOff(roomName);
-      powerResult = 'OFF';
-    }
-    const response = generatePowerUpdateResp(
-      powerResult,
-      endpointId,
-      requestToken,
-    );
-    log('DEBUG', 'Alexa.PowerController ', JSON.stringify(response));
-    context.succeed(response);
-  }
-
-  async function handleStateReport(
-    request: Alexa.API.Request,
-    context: Alexa.API.RequestContext,
-  ): Promise<void> {
-    const correlationToken = request.directive.header.messageId;
-    const endpointId = request.directive.endpoint?.endpointId || '';
-    const roomName = getRoomNameFromEndpointId(endpointId);
-    if (!roomName) {
-      throw new Error('unable to map endpointId to room name');
-    }
-    const roomState = await getRoomState(roomName);
-
-    const response = generateStateReportResponse(
-      roomState,
-      endpointId,
-      correlationToken,
-    );
-    context.succeed(response);
-  }
+  cleanseTokenFromRequest(request);
+  log('DEBUG:', 'handled request ', JSON.stringify(request));
 }
